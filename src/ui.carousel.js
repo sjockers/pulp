ad9315da322b48137f1d474fa87579130b5c3609
Simon Jockers <hello@simonjockers.com>
2012-07-17 09:26:10 +0200

    improved examples

--- src/ui.carousel.js
+++ src/ui.carousel.js
@@ -29,34 +29,33 @@
 		$containers[0] = carousel.$element.find("#container-0");
 		$containers[1] = carousel.$element.find("#container-1");
 		$containers[2] = carousel.$element.find("#container-2");
-		
-		slider = new pulp.util.Scroll('pulp-carousel', {
+
+
+		slider = new pulp.util.Scroll("pulp-carousel", {
 			snap: true,
 			momentum: false,
 			hScrollbar: false,
 			vScroll: false,
-			onScrollEnd: updateViews,
-			onBeforeScrollStart: function(){
-				
-				// TODO: Use this to flag if next/previous movement is possible!
-				
-				if(this.dirX > 0 && articles.hasNext()) {
-					pulp.log("HAS NEXT!");
-					//this.pastPageX = this.currPageX;
-					
-				}
-				if(this.dirX < 0 && articles.hasPrevious()) {
-					pulp.log("HAS PREV!");
-					//this.pastPageX = this.currPageX;
-					
-				}
-			}
-		});
-
-		$(window).bind('orientationchange resize', function () {
-			resizeContainers();
+			onScrollEnd: updateViews
 		});
 		
+		$("#pulp-next").click( function(){
+			console.log("YEAH!");
+			// slider.scrollToPage('next', 0);
+			// return false;
+		})
+		
+		$("#pulp-prev").click( function(){
+			console.log("YEAH!");
+			// slider.scrollToPage('prev', 0);
+			// return false;
+		})
+
+			
+		$(window).bind("orientationchange resize", function (){
+			resizeContainers();
+		});	
+				
 		resizeContainers();	
 	}
 	
@@ -70,56 +69,59 @@
 	}
 
 	function updateViews(e){
-
-		pulp.log(slider.pastPageX, slider.currPageX);
-		
-		// TODO: Remove if this proves unnecessary!
-		// if(slider.pastPageX == slider.currPageX){
-		// 	return;
-		// };
 		
 		if(slider.currPageX > 1) {
 			stepForward(carousel.views);
-		};
+		}
 		if(slider.currPageX < 1) {
 			stepBackward(carousel.views);						
-		};			
+		}
 
 	}	
 	
 	function stepForward(vws) {
-			
-		console.log(articles.hasNext());	
-			
+					
 		if(articles.hasNext()){
-			slider.center();
-
-			vws.previous = vws.current;
-			vws.current = vws.next;
-			vws.previous.render($containers[0]);
-			vws.current.render($containers[1]);
-			
-			scroller = new pulp.util.Scroll('container-1');
-							
+										
 			pulp.app.nextArticle();
-			vws.next = new pulp.ArticleView(articles.next(), $containers[2]);
+			
+			if(articles.hasNext()){
+				vws.previous = vws.current;
+				vws.current = vws.next;
+				
+				vws.previous.render($containers[0]);
+				vws.current.render($containers[1]);
+				slider.center();
+				scroller = new pulp.util.Scroll('container-1');
+				vws.next = new pulp.ArticleView(articles.next(), $containers[2]);
+			} 
+			else {
+				pulp.app.previousArticle();
+				vws.previous = new pulp.ArticleView(articles.previous(), $containers[0]);
+			}
 		}
 	}
 	
 	function stepBackward(vws) {
 
 		if(articles.hasPrevious()){
-			slider.center();
-		
-			vws.next = vws.current;
-			vws.current = vws.previous;
-			vws.next.render($containers[2]);
-			vws.current.render($containers[1]);				
 
-			scroller = new pulp.util.Scroll('container-1');
-		
 			pulp.app.previousArticle();				
-			vws.previous = new pulp.ArticleView(articles.previous(), $containers[0]);
+
+			if(articles.hasPrevious()){
+				vws.next = vws.current;
+				vws.current = vws.previous;
+				
+				vws.next.render($containers[2]);
+				vws.current.render($containers[1]);
+				slider.center();			
+				scroller = new pulp.util.Scroll('container-1');		
+				vws.previous = new pulp.ArticleView(articles.previous(), $containers[0]);
+			}
+			else {
+				pulp.app.nextArticle();
+				vws.next = new pulp.ArticleView(articles.next(), $containers[2]);				
+			}
 		}		
 	}	
 			
@@ -140,16 +142,19 @@
 			
 			articles.find("url", path);
 			var views = this.views;
-						
+			
 			views.current =  new pulp.ArticleView(articles.current(), $containers[1]);
-			scroller = new pulp.util.Scroll('container-1');
+			scroller = new pulp.util.Scroll('container-1');	
 			
 			if(articles.hasPrevious()){
 				views.previous = new pulp.ArticleView(articles.previous(), $containers[0]);					
-			}					
+			}
+						
 			if(articles.hasNext()){
 				views.next = new pulp.ArticleView(articles.next(), $containers[2]);
 			}
+			
+			slider.center();			
 			
 		}
 	});
